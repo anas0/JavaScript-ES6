3ccd8c304a35e0d9c718bb8c84fba1689a40896f
anas0 <ashraful0a@gmail.com>
2022-11-04 22:32:55 +0600

    new keyword in fat arrow function

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-const searchInput = document.querySelector('.search');
+// new keyword in fat arrow function
+function Person(name){
+    this.name = name;
+}
 
-const display = document.querySelector('.result');
-
-const thanks = document.querySelector('.thanks');
-
-// Normal Function thats work perfectly
-// function show(){
-//     display.innerHTML = this.value;
-//     var self = this;
-//     setTimeout(function(){        
-//         thanks.innerHTML = `You have typed: ${self.value}`;
-//     }, 1000);
-// }
-
-// const show = () => {
-//     display.innerHTML = this.value;
-// };
-
-// searchInput.addEventListener('keyup', show);
+var bismi = new Person('Bismi');
 
 
-//Arrow function not work for this
-searchInput.addEventListener('keyup', () => {
-    display.innerHTML = this.value;
-});
+//Person is not a constructor (arrow function dose not work for new keyword)
+var  Person = (name) => {
+    this.name = name;
+}
+
+var bismi2 = new Person('Bismi');
